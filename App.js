a25c5ee234a769986cdc263a452d3efb931866b4
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-04 10:25:39 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserList from './components/UserList';  // Correct import path
+
+function App() {
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
