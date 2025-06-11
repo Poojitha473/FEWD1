69827cb8cad4a8fbf70df2d5bd41c337e4977d3c
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-11 11:14:13 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import UserList from './components/UserList';  // Correct import path
+
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
-    <div>
-      <UserList />
+    <div className="App">
+      <ContactUs/>
     </div>
   );
 }
