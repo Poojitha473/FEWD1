d08f81352691adee210a8ce5ac45e80e1f664000
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-18 10:49:12 +0530

    Add files via upload

---
+++ Contact1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact1 = () => {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Email: support@example.com</p>
+      <p>Phone: +91 9876543210</p>
+    </div>
+  );
+};
+
+export default Contact1;
