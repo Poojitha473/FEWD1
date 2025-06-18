8712db2ed60afefac97a5978cf14d9aa01300a46
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-18 10:47:24 +0530

    Add files via upload

---
+++ Footer1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer1 = () => {
+  return (
+    <footer style={{ background: '#333', color: 'white', padding: '10px', textAlign: 'center' }}>
+      <p>© 2025 My Website. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer1;
