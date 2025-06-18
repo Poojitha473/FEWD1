bf2c48f4a53788d27b70a4162e5dfbb2c674caf4
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-18 10:48:50 +0530

    Add files via upload

---
+++ Home1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home1 = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to the Home page of our React website.</p>
+    </div>
+  );
+};
+
+export default Home1;
