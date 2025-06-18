19e4293e09c7423f08041cd0496186c8973a2713
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-18 10:48:29 +0530

    Add files via upload

---
+++ About1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About1 = () => {
+  return (
+    <div>
+      <h2>About Us</h2>
+      <p>We are a team of passionate developers building awesome web applications.</p>
+    </div>
+  );
+};
+
+export default About1;
