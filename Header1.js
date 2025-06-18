e1d097c63aded65b1fdf3b9a13d3bed984474c2d
Poojitha473 <pothukanipoojitha@gmail.com>
2025-06-18 10:47:07 +0530

    Add files via upload

---
+++ Header1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header1 = () => {
+  return (
+    <header style={{ background: '#007bff', padding: '10px', color: 'white' }}>
+      <h1>My React Site</h1>
+    </header>
+  );
+};
+
+export default Header1;
